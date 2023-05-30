3ef9029ac8722a7e66d412f57343c0f3b7156b1d
Anthony Bon Louis Uy Cubillas <66798713+louisuy@users.noreply.github.com>
2023-05-30 17:26:06 +0400

    Setting up Sanity.

---
+++ app/projects/page.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import { getProjects } from '@/sanity/sanity-utils'
+import { Project } from '@/types/Project';
+
+export default async function Projects() {
+    const projects = await getProjects();
+
+    return (
+        <div>
+            <div className='flex flex-col sm:flex-row'>
+                {projects.map((projects: Project) => (
+                    <div key={projects._id}>
+                        <div className='p-2'>
+                            <Image
+                                src={projects.image}
+                                alt={projects.name}
+                                width={4000}
+                                height={4000}
+                                className='rounded-3xl aspect-video object-cover overflow-hidden sm:aspect-square'
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
